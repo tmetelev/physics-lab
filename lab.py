9800847f6de084c6c51b8b041187ae0175968263
Timofei Metelev <t.metelew@gmail.com>
2021-10-28 22:26:58 +0300

    add Formula and Tex Threshold

--- lab.py
+++ lab.py
@@ -26,13 +26,50 @@ def container_threshold(x, linear,  c, g=1, m=100):
     return sqrt(dsys ** 2 + dzone ** 2)
 
 
-# class Var:
-#     def __init__(self, x, threshold, symbol):
-#         self.x = x
-#         self.threshold = threshold
-#         self.symb_x = Symbol(symbol)
-#         self.symb_threashold = Symbol("\Delta " + symbol)
-#
-#     def __add__(self, other):
+class Formula:
+    def __init__(self, symbol):
+        self.symbol = symbol
+        self.value = {}
+        self.__buf1 = []
+        self.thresholds = {}
+        self.__buf2 = []
+        self.symbols = []
+        self.__count = 0
+        self.formula = ""
+        self.__thresholds_formula = ""
 
+    def add(self, symbol, value, threshold):
+        self.__count += 1
+        self.symbols.append(symbol)
+        self.__buf1.append(value)
+        self.value = {self.symbols[i]:  self.__buf1[i] for i in range(self.__count)}
+        self.__buf2.append(threshold)
+        self.thresholds = {self.symbols[i]: self.__buf2[i] for i in range(self.__count)}
 
+    def count(self):
+        form = simplify(self.formula)
+        return form.evalf(subs=self.value)
+
+    def get_tex(self):
+        return self.symbol + " = " +  latex(simplify(self.formula))
+
+    def recount_threshold(self):
+        els = ""
+        for i in range(self.__count):
+            el = diff(self.formula, self.symbols[i])
+            d = Symbol("\Delta " + self.symbols[i])
+            el *= d
+            s1 = Symbol("(" + latex(el) + ")")
+            s1 **= 2
+            if i == 0:
+                els = s1
+            else:
+                els += s1
+        els = sqrt(els)
+        self.__thresholds_formula = els
+
+    def get_tex_threshold(self):
+        return "\Delta " + self.symbol + " = " + latex(self.__thresholds_formula)
+
+    def threshold(self):
+        pass
